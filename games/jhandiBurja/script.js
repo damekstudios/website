a035c4d6d27674f40a87661acdf98dd9f9503b1e
Damek Whiz <134848085+damekwhiz@users.noreply.github.com>
2023-11-13 09:54:21 +0545

    Add files

--- games/jhandiBurja/script.js
+++ games/jhandiBurja/script.js
@@ -9,11 +9,15 @@ const diceImages = [
 
 const diceDivs = document.getElementsByClassName('dice');
 const resetButton = document.getElementById('reset');
+const rollButton = document.getElementById('roll');
 const rollSound = new Audio('./img/dice-cup.wav');
+
+resetButton.disabled = true;
 
 function roll() {
     const diceArray = Array.from(diceDivs);
     // console.log(diceArray);
+    rollButton.disabled = true;
     resetButton.disabled = true;
     const interval = setInterval(() => {
         diceArray.forEach((dice) => {
@@ -27,9 +31,12 @@ function roll() {
 
     setTimeout(() => {
         clearInterval(interval);
+        rollSound.pause();
+        rollButton.disabled = false;
         resetButton.disabled = false;
     }, 5000);
 }
+
 
 function reset() {
     const diceArray = Array.from(diceDivs);
